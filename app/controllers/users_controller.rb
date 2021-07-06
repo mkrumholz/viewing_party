8d11dde1340c0f95aa526c0b31fb0ccac07ba6b7
Molly Krumholz <mkkrumholz@gmail.com>
2021-07-06 15:18:37 -0600

    Complete registration workflow

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,16 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    new_user = User.create(user_params)
+    flash[:success] = "Welcome, #{new_user.username}!"
+    redirect_to root_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end

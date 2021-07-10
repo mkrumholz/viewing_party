b4a5f2b897a6f96e23e0ba8dd41cfbba71d720fd
Molly Krumholz <mkkrumholz@gmail.com>
2021-07-10 10:04:58 -0600

    Create Movie PORO

---
+++ app/poros/movie.rb
@@ -0,0 +1,17 @@
+class Movie
+  attr_reader :title, :vote_average, :runtime, :genres, :summary, :reviews
+
+  def initialize(movie_details)
+    @title = movie_details[:title]
+    @vote_average = movie_details[:vote_average]
+    @runtime = movie_details[:runtime]
+    @genres = movie_details[:genres]
+    @summary = movie_details[:overview]
+    @cast = movie_details[:credits][:cast]
+    @reviews = movie_details[:reviews][:results]
+  end
+
+  def cast_members
+    @cast.first(10)
+  end
+end

90a3b93e92eec1106945ffdf15cbbd5156819363
Molly Krumholz <mkkrumholz@gmail.com>
2021-07-09 20:40:54 -0600

    Add functionality for exploring top 40 movies
    Co-authored-by: Emmy Morris <77904287+emmymorris@users.noreply.github.com>

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,27 @@
 class MoviesController < ApplicationController
   def index
-    @search_results = params[:results].first(40)
+    if params[:results]
+      @movies = params[:results]
+    else 
+      response_1 = Faraday.get('https://api.themoviedb.org/3/discover/movie') do |req|
+        req.params['api_key'] = ENV['MOVIE_DB_KEY']
+        req.params['sort_by'] = "popularity.desc"
+        req.params['include_adult'] = 'false'
+      end
+      json_1 = JSON.parse(response_1.body, symbolize_names: true)
+      search_results_page_1 = json_1[:results]
+  
+      response_2 = Faraday.get('https://api.themoviedb.org/3/discover/movie') do |req|
+        req.params['api_key'] = ENV['MOVIE_DB_KEY']
+        req.params['sort_by'] = "popularity.desc"
+        req.params['include_adult'] = 'false'
+        req.params['page'] = 2
+      end
+      json_2 = JSON.parse(response_2.body, symbolize_names: true)
+      search_results_page_2 = json_2[:results]
+
+      @movies = search_results_page_1 + search_results_page_2
+    end
   end
 
   def search
@@ -23,8 +44,8 @@ class MoviesController < ApplicationController
     json_2 = JSON.parse(response_2.body, symbolize_names: true)
     search_results_page_2 = json_2[:results]
 
-    @search_results = search_results_page_1 + search_results_page_2
+    search_results = search_results_page_1 + search_results_page_2
 
-    redirect_to movies_path(results: @search_results)
+    redirect_to movies_path(results: search_results)
   end
 end
--- spec/features/movies/index_spec.rb
+++ spec/features/movies/index_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe 'Movies index' do
       visit '/discover'
 
       response_body_1 = File.read('./spec/fixtures/search_results_1.json')
-      stub_request(:get, "https://api.themoviedb.org/3/search/movie?api_key=5aa90bee5771d3777a52a10d8dbc83ca&include_adult=false&language=en&query=Story")
+      stub_request(:get, "https://api.themoviedb.org/3/search/movie?api_key=#{ENV['MOVIE_DB_KEY']}&include_adult=false&language=en&query=Story")
           .with(
             headers: {
             'Accept'=>'*/*',
@@ -19,7 +19,7 @@ RSpec.describe 'Movies index' do
           .to_return(status: 200, body: response_body_1, headers: {})
 
       response_body_2 = File.read('./spec/fixtures/search_results_2.json')
-      stub_request(:get, "https://api.themoviedb.org/3/search/movie?api_key=5aa90bee5771d3777a52a10d8dbc83ca&include_adult=false&language=en&query=Story&page=2")
+      stub_request(:get, "https://api.themoviedb.org/3/search/movie?api_key=#{ENV['MOVIE_DB_KEY']}&include_adult=false&language=en&query=Story&page=2")
           .with(
             headers: {
             'Accept'=>'*/*',
@@ -46,5 +46,51 @@ RSpec.describe 'Movies index' do
     end
   end
 
-  it 'can display the top 40 movies'
+  describe 'top 40 movies' do
+    before :each do
+      user = User.create(username: 'test_user', email: 'user@test.com', password: 'test_password', password_confirmation: 'test_password')
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      response_body_1 = File.read('./spec/fixtures/top_40_1.json')
+      stub_request(:get, "https://api.themoviedb.org/3/discover/movie?api_key=#{ENV['MOVIE_DB_KEY']}&include_adult=false&sort_by=popularity.desc")
+          .with(
+            headers: {
+            'Accept'=>'*/*',
+            'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+            'User-Agent'=>'Faraday v1.4.1'
+            })
+          .to_return(status: 200, body: response_body_1, headers: {})
+      
+      response_body_2 = File.read('./spec/fixtures/top_40_2.json')
+      stub_request(:get, "https://api.themoviedb.org/3/discover/movie?api_key=#{ENV['MOVIE_DB_KEY']}&include_adult=false&sort_by=popularity.desc&page=2")
+          .with(
+            headers: {
+            'Accept'=>'*/*',
+            'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+            'User-Agent'=>'Faraday v1.4.1'
+            })
+          .to_return(status: 200, body: response_body_2, headers: {})
+    end
+
+    it 'can display the top 40 movies' do
+      visit '/discover'
+      click_on 'Discover Top 40 Movies'
+      
+      expect(current_path).to eq movies_path
+      expect(page).to have_content "The Boss Baby: Family Business"
+      expect(page).to have_content "Vote Average: 8"
+      expect(page).to have_content "Xtreme"
+      expect(page).to have_content "Vote Average: 7"
+    end
+
+    it "has a link to discover top 40 movies" do
+      visit '/movies'
+
+      expect(page).to have_button('Discover Top 40 Movies')
+
+      click_on 'Discover Top 40 Movies'
+
+      expect(current_path).to eq('/movies')
+    end
+  end
 end

453144ba73f65faa7177e0942b61a19dc833af7d
Molly Krumholz <mkkrumholz@gmail.com>
2021-07-11 21:56:37 -0600

    Refactor MovieDBService methods

--- app/services/movie_db_service.rb
+++ app/services/movie_db_service.rb
@@ -5,7 +5,42 @@ class MovieDbService
       req.params['language'] = 'en'
       req.params['append_to_response'] = 'credits,reviews'
     end
-    movie_details = JSON.parse(response.body, symbolize_names: true)
-    Movie.new(movie_details)
+    Movie.new(parse_json(response))
+  end
+
+  def self.top_40
+    response1 = Faraday.get('https://api.themoviedb.org/3/discover/movie') do |req|
+      req.params['api_key'] = ENV['MOVIE_DB_KEY']
+      req.params['sort_by'] = 'popularity.desc'
+      req.params['include_adult'] = 'false'
+    end
+    response2 = Faraday.get('https://api.themoviedb.org/3/discover/movie') do |req|
+      req.params['api_key'] = ENV['MOVIE_DB_KEY']
+      req.params['sort_by'] = 'popularity.desc'
+      req.params['include_adult'] = 'false'
+      req.params['page'] = 2
+    end
+    parse_json(response1)[:results] + parse_json(response2)[:results]
+  end
+
+  def self.search_results(title)
+    response1 = Faraday.get('https://api.themoviedb.org/3/search/movie') do |req|
+      req.params['api_key'] = ENV['MOVIE_DB_KEY']
+      req.params['language'] = 'en'
+      req.params['include_adult'] = 'false'
+      req.params['query'] = title
+    end
+    response2 = Faraday.get('https://api.themoviedb.org/3/search/movie') do |req|
+      req.params['api_key'] = ENV['MOVIE_DB_KEY']
+      req.params['language'] = 'en'
+      req.params['include_adult'] = 'false'
+      req.params['query'] = title
+      req.params['page'] = 2
+    end
+    parse_json(response1)[:results] + parse_json(response2)[:results]
+  end
+
+  def self.parse_json(response)
+    JSON.parse(response.body, symbolize_names: true)
   end
 end

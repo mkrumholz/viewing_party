96a8afaf1a64e0524330f8999d762f2302b5a819
Molly Krumholz <mkkrumholz@gmail.com>
2021-07-11 21:57:30 -0600

    Create AuthController and update Movie show page testing accordingly

---
+++ app/controllers/authorization_controller.rb
@@ -0,0 +1,10 @@
+class AuthorizationController < ApplicationController
+  before_action :authorize_user
+
+  private 
+
+  def authorize_user
+    redirect_to root_path unless current_user
+    flash[:error] = "Error: Please log in to view this content."
+  end
+end
--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,26 +1,9 @@
-class MoviesController < ApplicationController
+class MoviesController < AuthorizationController
   def index
     if params[:results]
       @movies = params[:results]
     else
-      response1 = Faraday.get('https://api.themoviedb.org/3/discover/movie') do |req|
-        req.params['api_key'] = ENV['MOVIE_DB_KEY']
-        req.params['sort_by'] = 'popularity.desc'
-        req.params['include_adult'] = 'false'
-      end
-      json1 = JSON.parse(response1.body, symbolize_names: true)
-      search_results_page1 = json1[:results]
-
-      response2 = Faraday.get('https://api.themoviedb.org/3/discover/movie') do |req|
-        req.params['api_key'] = ENV['MOVIE_DB_KEY']
-        req.params['sort_by'] = 'popularity.desc'
-        req.params['include_adult'] = 'false'
-        req.params['page'] = 2
-      end
-      json2 = JSON.parse(response2.body, symbolize_names: true)
-      search_results_page2 = json2[:results]
-
-      @movies = search_results_page1 + search_results_page2
+      @movies = MovieDbService.top_40
     end
   end
 
@@ -29,27 +12,7 @@ class MoviesController < ApplicationController
   end
 
   def search
-    response1 = Faraday.get('https://api.themoviedb.org/3/search/movie') do |req|
-      req.params['api_key'] = ENV['MOVIE_DB_KEY']
-      req.params['language'] = 'en'
-      req.params['include_adult'] = 'false'
-      req.params['query'] = params[:title]
-    end
-    json1 = JSON.parse(response1.body, symbolize_names: true)
-    search_results_page1 = json1[:results]
-
-    response2 = Faraday.get('https://api.themoviedb.org/3/search/movie') do |req|
-      req.params['api_key'] = ENV['MOVIE_DB_KEY']
-      req.params['language'] = 'en'
-      req.params['include_adult'] = 'false'
-      req.params['query'] = params[:title]
-      req.params['page'] = 2
-    end
-    json2 = JSON.parse(response2.body, symbolize_names: true)
-    search_results_page2 = json2[:results]
-
-    search_results = search_results_page1 + search_results_page2
-
+    search_results = MovieDbService.search_results(params[:title])
     redirect_to movies_path(results: search_results)
   end
 end

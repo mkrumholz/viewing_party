2100a038e0627218031a650db07d5e110ef39da3
Emmy Morris <emmymorris1102@gmail.com>
2021-07-13 16:03:16 -0600

    Run rubocop

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -9,27 +9,29 @@ class PartiesController < ApplicationController
   def create
     new_party = current_user.parties.new(party_params)
     if params[:party][:duration] >= params[:runtime] && new_party.save && params[:party][:invitations].present? && params[:external_movie_id].present?
-       if params[:party][:invitations].any? {|invitation| invitation != ""}
+      if params[:party][:invitations].any? { |invitation| invitation != '' }
         params[:party][:invitations].each do |invitation|
-          new_party.invitations.create(user_id: invitation) if invitation != ""
+          new_party.invitations.create(user_id: invitation) if invitation != ''
         end
-       end
-      flash[:success] = "New Viewing Party Created"
+      end
+      flash[:success] = 'New Viewing Party Created'
       redirect_to dashboard_path
-    elsif !params[:party][:invitations].present?
-      flash[:error] = "Error: Party must need friends."
-      redirect_to new_party_path({title: params[:party][:movie_title], duration: params[:runtime]})
+    elsif params[:party][:invitations].blank?
+      flash[:error] = 'Error: Party must need friends.'
+      redirect_to new_party_path({ title: params[:party][:movie_title], duration: params[:runtime] })
     elsif params[:party][:duration] < params[:runtime] && params[:party][:invitations].present?
-      flash[:error] = "Error: Party duration must match or exceed movie runtime."
-      redirect_to new_party_path({title: params[:party][:movie_title], duration: params[:runtime]})
+      flash[:error] = 'Error: Party duration must match or exceed movie runtime.'
+      redirect_to new_party_path({ title: params[:party][:movie_title], duration: params[:runtime] })
     else
-      flash[:error] = "Error: Party not created"
+      flash[:error] = 'Error: Party not created'
       render :new
     end
   end
 
   private
+
   def party_params
-    params.require(:party).permit(:movie_title, :duration, :date, :start_time).merge({external_movie_id: params[:external_movie_id]})
+    params.require(:party).permit(:movie_title, :duration, :date,
+                                  :start_time).merge({ external_movie_id: params[:external_movie_id] })
   end
 end

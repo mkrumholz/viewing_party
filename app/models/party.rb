3b2f08e4cb90116befd2ab1d7d887c57c1ff3de4
Molly Krumholz <mkkrumholz@gmail.com>
2021-07-14 18:14:26 -0600

    Make emails send on Party creation to first guest

--- app/models/party.rb
+++ app/models/party.rb
@@ -7,4 +7,10 @@ class Party < ApplicationRecord
   validates :date, presence: true
   validates :start_time, presence: true
   validates :external_movie_id, presence: true
+
+  def send_invitations
+    host = User.find(user_id)
+    friend = User.find(invitations.first.user_id)
+    InvitationMailer.invite(host, friend, self).deliver_now
+  end
 end
